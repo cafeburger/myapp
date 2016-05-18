445653a2624caefd692c926b6db1d58a15b682a6
SSSamLiu <sam.liu@mail.searching-service.com>
2016-05-18 16:11:31 +0800

    add projects

---
+++ client/app.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import "zone.js/dist/zone";
+import {Component} from "@angular/core";
+import {bootstrap} from "angular2-meteor-auto-bootstrap";
+import {Parties} from "../collections/parties";
+import {Mongo} from "meteor/mongo";
+import {PartiesForm} from "./imports/parties-form/parties-form";
+import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from '@angular/router-deprecated';
+
+@Component({
+  selector: 'app',
+  templateUrl: 'client/app.html',
+  directives: [PartiesForm, ROUTER_DIRECTIVES]
+})
+class Socially {
+  parties:Mongo.Cursor;
+
+  constructor() {
+    this.parties = Parties.find();
+  }
+
+  removeParty(party) {
+    Parties.remove(party._id);
+  }
+}
+
+bootstrap(Socially,[ROUTER_PROVIDERS]);
---
+++ client/imports/parties-form/parties-form.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import {Component} from "@angular/core";
+import {FormBuilder, ControlGroup, Validators} from "@angular/common";
+import {Parties} from "../../../collections/parties.ts";
+
+@Component({
+  selector: 'parties-form',
+  templateUrl: '/client/imports/parties-form/parties-form.html'
+})
+export class PartiesForm {
+  partiesForm:ControlGroup;
+
+  constructor() {
+    let fb = new FormBuilder();
+    this.partiesForm = fb.group({
+      name: ['Bob', Validators.required],
+      description: [''],
+      location: ['', Validators.required]
+    });
+  }
+
+  addParty(party) {
+    if (this.partiesForm.valid) {
+      Parties.insert({
+        name: party.name,
+        descriptyion: party.description,
+        location: party.location
+      })
+
+
+    }
+    (this.partiesForm.controls['name']).updateValue('');
+
+    (this.partiesForm.controls['description']).updateValue('');
+    (this.partiesForm.controls['location']).updateValue('');
+  }
+
+}
